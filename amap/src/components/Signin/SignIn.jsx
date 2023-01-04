9abe7d5b943dc65f38d464d037ec847788cec79b
Arnaud B <arnaud.bourgoin@outlook.com>
2023-01-04 17:08:24 +0100

    Redirection vers la homePage après connexion

--- amap/src/components/Signin/SignIn.jsx
+++ amap/src/components/Signin/SignIn.jsx
@@ -1,46 +1,46 @@
 import { useState } from "react";
-
+import { useNavigate } from "react-router-dom";
 
 function SignIn() {
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
 
-    const [user, setUser] = useState({
-        username: "",
-        password: ""
-    })
+  const navigate = useNavigate();
 
-    const handleChange = (event) => {
-        setUser(() => ({
-                ...user,
-                [event.target.name]: event.target.value,
-              }));
-        }
+  const handleChange = (event) => {
+    setUser(() => ({
+      ...user,
+      [event.target.name]: event.target.value,
+    }));
+  };
 
-        const handleSubmit = (event) => {
-            event.preventDefault();
-            console.log(user);
-        }
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    navigate("/");
+  };
 
-    return (
-        <div className='login'>
-            <h1>LOG IN</h1>
-            <form onSubmit={handleSubmit}>
-                <label>
-                    <p>Username:</p>
-                    <input type="text" name="username" onChange={handleChange} />
-                </label>
-                <label>
-                    <p>Password:</p>
-                    <input type="password" name='password' onChange={handleChange} />
-                </label>
-                <div>
-                    <button type="submit">Submit</button>
-                </div>
-            </form>
-        </div>
-    )
-
-
-
+  return (
+    <div className="login">
+      <h1>LOGIN</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="email"
+          placeholder="Exemple@gmail.com"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          name="password"
+          onChange={handleChange}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 }
 
 export default SignIn;
