59b22f9e6cc22bde771f45547334a099935290b5
AndriyLeb <andriy.lebedev4@etu.univ-lorraine.fr>
2023-01-05 15:59:43 +0100

    pages

---
+++ amap/src/pages/GrowersPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Growers from '../components/Growers/Growers';
+
+function GrowersPage() {
+
+  return (
+    <>
+    <header>
+      <div></div>
+    </header>
+    <Growers />
+    </>
+  );
+}
+
+export default GrowersPage
