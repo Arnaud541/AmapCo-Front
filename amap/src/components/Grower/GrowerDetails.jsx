234139d90e9026f385300726070aac5e0bd18c50
SEBonduel <sebastien050599@hotmail.fr>
2023-02-07 16:04:06 +0100

    petite pause

--- amap/src/components/Grower/GrowerDetails.jsx
+++ amap/src/components/Grower/GrowerDetails.jsx
@@ -1,15 +1,35 @@
 import React from "react";
-import "./Growers.css";
+import "./GrowerDetails.css";
+import growerimg from  "../../assets/img/backgroundgrower.png"
 
 function GrowerDetails(props) {
   const{grower}=props
   console.log(grower)
     return (
     <div className="container">
-    <h1>hello</h1>
-    <h2>
-              {grower.nom}
-                </h2>
+      
+      <img id="growerimg" src={growerimg}/>
+
+      <div className="growerinfo">
+        <h1>div1 remplir les infos du grower</h1>
+        {grower.avatar}
+        {grower.nom}
+        {grower.created_at}
+      </div>
+
+      <div className="growercart">
+        <h1>div2 paniers de producteur</h1>
+      </div>
+
+      
+      <div className="userReview">
+        <h2 id="titleUserReview">Avis des amapiens</h2>
+        <hr />
+      </div>
+      
+
+
+
     </div>
     
   )
--- amap/src/pages/GrowerDetailsPage.jsx
+++ amap/src/pages/GrowerDetailsPage.jsx
@@ -3,14 +3,17 @@ import GrowerDetails from '../components/Grower/GrowerDetails';
 import { useEffect,useState } from 'react';
 import Navbar from '../components/Navbar/Navbar';
 import axios from 'axios';
+import { useParams } from 'react-router-dom';
 
 function GrowerDetailsPage() {
     const [grower, setGrower] = useState({});
-  
+    const {id}=useParams();
     useEffect(() => {
       axios
-        .get("http://127.0.0.1/AmapCo-Back/index.php?action=GrowerById")
+        .get("http://127.0.0.1/AmapCo-Back/index.php?action=GrowerById",{params:{id}})
         .then((response) => {
+          console.log(response.data.producteur)
+          console.log(response)
           setGrower(response.data.producteur[0]);
         });
     }, []);
