74e82f20fc9a48666063ed58e217eac51ee1011f
PaulGillon <pau.gillon0@gmail.com>
2023-02-08 14:49:54 +0100

    Modif pages/SignUpPage

--- amap/src/pages/SignUpPage.jsx
+++ amap/src/pages/SignUpPage.jsx
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useEffect } from "react";
 import SignUp from '../components/Signup/SignUp';
 
 
 function SignUpPage() {
+
+    useEffect(() => {
+        axios
+          .post("http://127.0.0.1/AmapCo-Back/index.php?action=user")
+          .then((response) => {
+            console.log(response);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      }, []);
+
     return (
         <>
             <SignUp />
