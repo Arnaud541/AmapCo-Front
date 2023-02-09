18e6c3cffa9d399d236bdaebc6ec83d27f71742f
SEBonduel <sebastien050599@hotmail.fr>
2023-02-09 11:21:13 +0100

    grower review works

--- amap/src/components/Grower/GrowerDetails.jsx
+++ amap/src/components/Grower/GrowerDetails.jsx
@@ -4,14 +4,14 @@ import growerimg from  "../../assets/img/backgroundgrower.png"
 
 
 function GrowerDetails(props) {
-  const{grower, growercart, growerriew}=props
+  const{grower, growercart, growerreview}=props
   console.log(growercart)
     return (
     <div className="container">
       
       <img id="growerimg" src={growerimg}/>
       <div className="growerinfo">
-        <div className="groweravatar">{grower.avatar}</div>
+        <img src={"{grower.avatar}"}alt="Avatar"></img>
         <div className="growerdata"><p className="data">{grower.nom}<br></br>Date d'inscription {grower.created_at}</p></div>
       </div>
 
@@ -19,7 +19,7 @@ function GrowerDetails(props) {
 
         {growercart.map((cart) => (
           <div className="growerCart">
-              {cart.img_url}
+              <img src={"cart.img_url"}alt="CartPicture"></img>
               <div className="titlewithgrowercart">
                 <h3 className="titlecart">{cart.nom}</h3>
                 <h4 className="title4grower">{grower.nom}</h4>
@@ -37,14 +37,14 @@ function GrowerDetails(props) {
         <h2 id="titleUserReview">Avis des amapiens</h2>
         <hr />
 
-        {/* {growerreview.map((gr) => (
+        {growerreview.map((gr) => (
           
           <div className="growerReview">
-            
-              <h2>{gr.avis}</h2>
+              <p className="note">{gr.note}</p>
+              <p className="growerReviews">{gr.avis}</p>
           </div>
             
-        ))} */}
+        ))}
       </div>
     </div>   
   )
