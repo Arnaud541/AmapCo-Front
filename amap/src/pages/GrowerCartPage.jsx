76d2af7e5f2b0d539c95b4fcec9a61ed5177fcad
Arnaud B <arnaud.bourgoin@outlook.com>
2023-02-14 16:01:57 +0100

    fdsfds

--- amap/src/pages/GrowerCartPage.jsx
+++ amap/src/pages/GrowerCartPage.jsx
@@ -1,39 +1,32 @@
 import React from "react";
-import { useEffect,useState } from 'react';
+import { useEffect, useState } from "react";
 import Navbar from "../components/Navbar/Navbar";
-import GrowerCart from "../components/Grower/GrowerCart"
-import axios from 'axios';
+import GrowerCart from "../components/Grower/GrowerCart";
+import axios from "axios";
 import { useParams } from "react-router-dom";
-  
-  function GrowerCartPage() {
-    const {id} = useParams();
-    const [cartDetails, setCart] = useState([]);
-    useEffect(() => {
+
+function GrowerCartPage() {
+  const [cartDetails, setCartDetails] = useState([]);
+  const { id } = useParams();
+  useEffect(() => {
     axios
-        .get("http://127.0.0.1/AmapCo-Back/index.php?action=cartDetails",{
-          params:{
-            id
-          }
-        })
-        .then((response) => {
-          console.log(response)
-          setCart(response.data.detail);
-        });
-
-
-
-
-
-
-
-      },[]);
+      .get("http://127.0.0.1/AmapCo-Back/index.php?action=cartDetails", {
+        params: {
+          id,
+        },
+      })
+      .then((response) => {
+        if (response.data.status === 200) {
+          console.log(response.data.detail);
+          setCartDetails(response.data.detail);
+        }
+      });
+  }, []);
   return (
     <>
-    <Navbar />
-      <GrowerCart cartDetails={cartDetails}/>
+      <Navbar />
+      <GrowerCart cartDetails={cartDetails} />
     </>
-      
-
   );
 }
 
