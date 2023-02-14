b853994ce38bd825799f88e0435b37b9a92f9de9
SEBonduel <sebastien050599@hotmail.fr>
2023-02-14 16:59:57 +0100

    style cart css

--- amap/src/components/Grower/GrowerCart.jsx
+++ amap/src/components/Grower/GrowerCart.jsx
@@ -6,36 +6,46 @@ function GrowerCart(props) {
   return (
     <div className="container">
       <div className="cart">
+        <h1 className="Cart-Name">{cartDetails[0]?.PanierProducteurNom}</h1>
         <div className="img-cart">
-          <img id="cartimg" src={cartDetails[0]?.img_url} alt="CartPicture" />
-          <h3>
-            {"Panier de " +
+          <img className="cartimg" src={cartDetails[0]?.img_url} alt="CartPicture" />
+          <h3 className="growername">
+            {"Panier proposé par " +
               cartDetails[0]?.ProducteurNom +
               " " +
               cartDetails[0]?.prenom}
           </h3>
+        
         </div>
-        <div className="cart_products">
-          <h2>Produits</h2>
+        
+            <h3 className="titldesc">Description</h3>
+            <hr></hr>
+          <div className="descriptioncart">
+          <span className="description">{cartDetails[0]?.description}</span>
+          
+          </div>
+        
+          <h2 className="productsTitle">Produits</h2>
           <hr />
           <div className="carts-products-items">
             {cartDetails.map((c) => (
               <div className="cart_products_item">
                 <h4>
-                  {c.IngredientNom} : {c.quantite}
+                  <ul className="list">{c.IngredientNom} : {c.quantite}</ul>
                 </h4>
               </div>
             ))}
           </div>
         </div>
+        <div className="associatedRecipes">
+          <h3 className="titleAssociatedRecipes">Recettes associées</h3>
+            <hr></hr>
 
-        {/* {
-        cartDetails.map((c) => (
-          <div>{c.PanierProducteurNom}</div>
-        ))
-      }  */}
+
+              
+        </div>
+
       </div>
-    </div>
   );
 }
 
