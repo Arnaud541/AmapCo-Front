90474c964e0066080fe7eb17baa53ecfc4066dcf
Arnaud B <arnaud.bourgoin@outlook.com>
2023-02-15 15:40:40 +0100

    indentation

--- amap/src/components/SearchBarCarts/SearchBarCarts.jsx
+++ amap/src/components/SearchBarCarts/SearchBarCarts.jsx
@@ -5,121 +5,115 @@ import { FaSearch, FaSlidersH } from "react-icons/fa";
 import { useState } from "react";
 import axios from "axios";
 
-
-
-
-
 function SearchBarCarts(props) {
-    const {setCart} = props
+  const { setCart } = props;
 
-    const type = [
-      { value: "fruit", nom: "fruit" },
-      { value: "légume", nom: "légume" },
-      { value: "viande", nom: "viande" },
-      { value: "mélange", nom: "mélange" },
-    ];
-  
-    const [search, setSearch] = useState({
-      search: "",
-      filters: {
-        type: [],
-      },
-    });
-  
-    const [showFilters, setShowFilters] = useState(false);
-    const animatedComponents = makeAnimated();
+  const type = [
+    { value: "fruit", nom: "fruit" },
+    { value: "légume", nom: "légume" },
+    { value: "viande", nom: "viande" },
+    { value: "mélange", nom: "mélange" },
+  ];
 
+  const [search, setSearch] = useState({
+    search: "",
+    filters: {
+      type: [],
+    },
+  });
 
+  const [showFilters, setShowFilters] = useState(false);
+  const animatedComponents = makeAnimated();
 
-    const handleSubmit = (event) => {
-      event.preventDefault();
-      axios
-        .get("http://127.0.0.1/AmapCo-Back/index.php?action=CartSearch", {
-          params: {
-            search: search,
-          },
-        })
-        .then((response) => { console.log(response.data.producerCart)
-          setCart(response.data.producerCart);
-        });
-    };
-  
-    const handleFilters = (data, event) => {
-      if (event.name.startsWith("filter")) {
-        let copySearch = { ...search };
-        let array = [];
-  
-        data.forEach((e) => {
-          array.push(e.nom);
-        });
-        
-        let filtre = event.name.split("-")[1];
-        switch (filtre) {
-          case "type":
-            copySearch.filters.type = array;
-            break;
-        }
-        setSearch(copySearch);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios
+      .get("http://127.0.0.1/AmapCo-Back/index.php?action=CartSearch", {
+        params: {
+          search: search,
+        },
+      })
+      .then((response) => {
+        console.log(response.data.producerCart);
+        setCart(response.data.producerCart);
+      });
+  };
+
+  const handleFilters = (data, event) => {
+    if (event.name.startsWith("filter")) {
+      let copySearch = { ...search };
+      let array = [];
+
+      data.forEach((e) => {
+        array.push(e.nom);
+      });
+
+      let filtre = event.name.split("-")[1];
+      switch (filtre) {
+        case "type":
+          copySearch.filters.type = array;
+          break;
       }
-    };
-  
-    const handleChange = (event) => {
-      setSearch(() => ({
-        ...search,
-        [event.target.name]: event.target.value,
-      }));
-    };
-  
-    const handleClick = () => {
-      setShowFilters(!showFilters);
-    };
-  
-    const customStyles = {
-      option: (provided, state) => ({
-        ...provided,
-        borderBottom: "1px dotted pink",
-        color: state.isSelected ? "red" : "blue",
-        padding: 20,
-        width: 200,
-        height: 20,
-      }),
-      container: (styles) => ({ ...styles, width: "200px" }),
-    };
-  
-    return (
-      <form className="search" onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Votre recherche"
-          onChange={handleChange}
-          name="search"
-        />
-        <button type="submit">
-          <FaSearch />
-        </button>
-        
-        <button type="button" onClick={handleClick}>
-          <FaSlidersH />
-        </button>
-        {showFilters ? (
-          <div className="filters">
+      setSearch(copySearch);
+    }
+  };
 
-            <Select
-              closeMenuOnSelect={false}
-              components={animatedComponents}
-              isMulti
-              options={type}
-              getOptionLabel={(option) => option.nom}
-              getOptionValue={(option) => option.value}
-              onChange={handleFilters}
-              placeholder="type"
-              name="filter-type"
-              styles={customStyles}
-            />
-          </div>
-        ) : null}
-      </form>
-    );
-  }
-  
-  export default SearchBarCarts;
+  const handleChange = (event) => {
+    setSearch(() => ({
+      ...search,
+      [event.target.name]: event.target.value,
+    }));
+  };
+
+  const handleClick = () => {
+    setShowFilters(!showFilters);
+  };
+
+  const customStyles = {
+    option: (provided, state) => ({
+      ...provided,
+      borderBottom: "1px dotted pink",
+      color: state.isSelected ? "red" : "blue",
+      padding: 20,
+      width: 200,
+      height: 20,
+    }),
+    container: (styles) => ({ ...styles, width: "200px" }),
+  };
+
+  return (
+    <form className="search" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Votre recherche"
+        onChange={handleChange}
+        name="search"
+      />
+      <button type="submit">
+        <FaSearch />
+      </button>
+
+      <button type="button" onClick={handleClick}>
+        <FaSlidersH />
+      </button>
+      {showFilters ? (
+        <div className="filters">
+          <Select
+            closeMenuOnSelect={false}
+            components={animatedComponents}
+            isMulti
+            options={type}
+            getOptionLabel={(option) => option.nom}
+            getOptionValue={(option) => option.value}
+            onChange={handleFilters}
+            placeholder="type"
+            name="filter-type"
+            styles={customStyles}
+          />
+        </div>
+      ) : null}
+    </form>
+  );
+}
+
+export default SearchBarCarts;
--- amap/src/pages/HomePage.jsx
+++ amap/src/pages/HomePage.jsx
@@ -1,27 +1,26 @@
 import React from "react";
 import Navbar from "../components/Navbar/Navbar";
-import SearchBarCarts from "../components/SearchBarCarts/SearchBarCarts"
+import SearchBarCarts from "../components/SearchBarCarts/SearchBarCarts";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import Carts from "../components/Home/Carts";
 
 function HomePage() {
+  const [carts, setCart] = useState([]);
 
-const [carts, setCart] = useState([]);
-
-useEffect(() => {
-  axios
-  .get("http://127.0.0.1/AmapCo-Back/index.php?action=producerCart")
-  .then((reponse) => {
-    setCart(reponse.data.producerCart)
-  });
-}, []);
+  useEffect(() => {
+    axios
+      .get("http://127.0.0.1/AmapCo-Back/index.php?action=producerCart")
+      .then((reponse) => {
+        setCart(reponse.data.producerCart);
+      });
+  }, []);
 
   return (
     <>
-      <Carts carts={carts} />
-      <SearchBarCarts setCart={setCart} />
       <Navbar />
+      <SearchBarCarts setCart={setCart} />
+      <Carts carts={carts} />
     </>
   );
 }
