1d33b8c970a01e48d6014846e691af0a4690f3e7
Arnaud B <arnaud.bourgoin@outlook.com>
2023-02-17 17:31:03 +0100

    sdgdsg

--- amap/src/pages/FormRecipeEditPage.jsx
+++ amap/src/pages/FormRecipeEditPage.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import Navbar from "../components/Navbar/Navbar";
+import FormRecipeEdit from "../components/Recipes/FormRecipeEdit";
 
 function FormRecipeEditPage() {
   const { id } = useParams();
-  return <div>FormRecipeEditPage</div>;
+  return (
+    <>
+      <Navbar />
+      <FormRecipeEdit />
+    </>
+  );
 }
 
 export default FormRecipeEditPage;
